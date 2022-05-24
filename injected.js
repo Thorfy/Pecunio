d1ff3342ed11332c8feb8d7575d3fc75c27b30ca
Rémy Lefebvre <lefebvre.remy.pro@gmail.com>
2022-05-24 15:38:00 +0200

    Load Correct line of data, add style/color/misc

--- injected.js
+++ injected.js
@@ -8,6 +8,7 @@ let urlCategory = "/v2/categories?limit=200";
 
 let allTransactions = [];
 let allCategory = [];
+let preparedData = [];
 
 port.onMessage.addListener(message => {
     authHeader = message.data;
@@ -33,13 +34,12 @@ setInterval(function () {
     if (currentPage != location.href) {
         // page has changed, set new page as 'current'
         currentPage = location.href;
-       build();
+        build();
     }
 }, 500);
 
 
-
-function build(){
+function build() {
     if (location.href === "https://app2.bankin.com/accounts") {
         buildChart();
     }
@@ -61,26 +61,55 @@ function getBankinData(authHeader, domain, globalVar, url) {
                 data.resources.map(transaction => globalVar.push(transaction))
             }
             if (data.pagination.next_uri && data.pagination.next_uri.length) {
-                getBankinData(authHeader, domain, globalVar,data.pagination.next_uri);
+                getBankinData(authHeader, domain, globalVar, data.pagination.next_uri);
             }
         })
 }
 
+
 function buildChart() {
-    if (allTransactions) {
-        //select categroy DIV
-        let homeBlock = document.getElementsByClassName("homeBlock")
-        let canvasDiv = document.createElement('canvas');
-        if (homeBlock.length != 0) {
-            homeBlock[0].innerHTML = "";
-            homeBlock[0].appendChild(canvasDiv);
-        }
-        let chartJsConfig = getChartJsConfig();
-        chartJsConfig.data = getFakeData();
-        const ctx = canvasDiv.getContext('2d');
-        const myChart = new Chart(ctx, chartJsConfig);
+
+    let transactionByCategory = false;
+    if (allCategory.length && allTransactions.length) {
+        transactionByCategory = mergeTransactionByCategory(allTransactions, allCategory);
     }
+
+    //select categroy DIV
+    let homeBlock = document.getElementsByClassName("homeBlock")
+    let canvasDiv = document.createElement('canvas');
+    if (homeBlock.length != 0) {
+        homeBlock[0].innerHTML = "";
+        homeBlock[0].appendChild(canvasDiv);
+    }
+    let chartJsConfig = getChartJsConfig();
+    chartJsConfig.data = getRealData(allCategory, transactionByCategory);
+    const ctx = canvasDiv.getContext('2d');
+    const myChart = new Chart(ctx, chartJsConfig);
+
 }
+
+function mergeTransactionByCategory(allTransactions, allCategory) {
+    allTransactions.forEach(transaction => {
+        allCategory.forEach(category => {
+            if (!preparedData[category.id])
+                preparedData[category.id] = [];
+
+            if (transaction.category.id === category.id) {
+                preparedData[category.id].push(transaction);
+            } else {
+                category.categories.forEach(childCategory => {
+                    if (transaction.category.id === childCategory.id) {
+                        preparedData[category.id].push(transaction);
+                    }
+                })
+            }
+        })
+
+    })
+    //console.log(preparedData);
+    return preparedData;
+}
+
 
 function getChartJsConfig() {
 
@@ -102,7 +131,7 @@ function getChartJsConfig() {
                     grid: {
                         color: "#e9f5f9",
                         borderColor: "#d3eaf2",
-                        tickColor:"#e9f5f9"
+                        tickColor: "#e9f5f9"
                     },
                     display: true,
                     title: {
@@ -117,7 +146,7 @@ function getChartJsConfig() {
                         borderColor: "#d3eaf2",
                         tickColor: "#e9f5f9"
                     },
-                    
+
                     suggestedMin: -10,
                     suggestedMax: 200
                 }
@@ -127,14 +156,48 @@ function getChartJsConfig() {
     return chartJsConfig;
 }
 
+function getRealData(categoryData, transactionByCategory = false) {
+    //get config and data
+    //const DATA_COUNT = timeInterval();
+
+    let lengthMax = 0;
 
 
+    let data = {
+        datasets: []
+    }
+    categoryData.forEach(category => {
+        let transactions = transactionByCategory[parseInt(category.id)];
+        let dateValueObject = [];
+
+        // par mois faire addition de chaque transaction
+        transactions.forEach(transaction => {
+           dateValueObject.push({
+               x:transaction.date,
+               y:transaction.amount
+           })
+        })
+
+        dateValueObject.sort(function (a, b) {
+            let dateA = new Date(a.x), dateB = new Date(b.x)
+            return dateA - dateB
+        });
+        let dataCategory = {
+            label: category.name,
+            data: dateValueObject,
+            borderColor: parseColorCSS("categoryColor_" + category.id),
+            fill: false,
+            tension: 0.4
+        }
+
+        data.datasets.push(dataCategory);
+
+    })
+    return data
+}
 
 function getFakeData() {
-    console.log(allTransactions)
-    console.log(allCategory)
-
-//get config and data
+    //get config and data
     const DATA_COUNT = timeInterval();
     const labels = [];
     for (let i = 0; i < DATA_COUNT; ++i) {
@@ -148,13 +211,13 @@ function getFakeData() {
             {
                 label: 'Course',
                 data: datapoints,
-                borderColor: "yellow",
+                borderColor: parseColorCSS("categoryColor_162"),
                 fill: false,
                 tension: 0.4
             }, {
                 label: 'Voiture',
                 data: datapoints1,
-                borderColor: "blue",
+                borderColor: parseColorCSS("categoryColor_162"),
                 fill: false,
                 tension: 0.4
             }
@@ -168,6 +231,15 @@ function timeInterval() {
     //default val
     let val = 12;
     //get value of html input or button
-
     return val;
 };
+
+//trick for get real color of category
+function parseColorCSS(strClass) {
+    let styleElement = document.createElement("div");
+    styleElement.className = strClass;
+    document.body.appendChild(styleElement)
+    let colorVal = window.getComputedStyle(styleElement).backgroundColor;
+    styleElement.remove();
+    return colorVal;
+}
