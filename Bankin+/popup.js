5a71611705b7cbc90c9e917dfc1e27bf1aad5af3
Rémy Lefebvre <lefebvre.remy.pro@gmail.com>
2023-03-04 23:41:16 +0100

    Fix misc

--- Bankin+/popup.js
+++ Bankin+/popup.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("popupReady")
 
+    let currentVersionImg = document.querySelector("#currentVersion")
+    let cvSource = "https://img.shields.io/badge/installed-" + chrome.runtime.getManifest().version + "-blue"
+    currentVersionImg.src = cvSource
+
     let startDate = document.querySelector('#startDate');
     let endDate = document.querySelector('#endDate');
     let accounts = document.querySelector('#accounts');
-    
+
     chrome.storage.local.get(['startDate', 'endDate'], function (data) {
         for (const input of inputs) {
             input.value = data[input.id]
@@ -12,16 +16,16 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     chrome.storage.local.get(['accountsList'], function (data) {
-        for (const account of data.accountsList){
-            let newOption = new Option(account,account);
+        for (const account of data.accountsList) {
+            let newOption = new Option(account, account);
             accounts.add(newOption, undefined);
         }
     });
-    
-    
+
+
     let inputs = [startDate, endDate, accounts]
     for (const input of inputs) {
-        input.addEventListener('blur', function() {
+        input.addEventListener('blur', function () {
             console.log(this.id, this.value)
             chrome.storage.local.set({ [this.id]: this.value });
         })
