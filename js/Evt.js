e6d126a2371eac7d4601bb00d2673245093a3064
lefebvreRemy <remy.lefebvre@meteoconsult.com>
2023-08-05 20:48:15 +0200

    Allow multiple support for EVT (bg,popup,content)

--- js/Evt.js
+++ js/Evt.js
@@ -6,16 +6,33 @@ class Evt {
     constructor() {
         this.initPrivate();
     }
-    listen(str, cb, once = false) {
+    listen(str, cb, once = false, useChromeMessaging = false) {
         if(this._eventDispatched[str]) {
             cb();
         } else {
+            // Écoute les événements standards du document
             document.addEventListener(this._key + str, cb, { once:once });
+            
+            // Si useChromeMessaging est vrai, écoute aussi les messages de Chrome
+            if(useChromeMessaging) {
+                chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+                    if (request.method === this._key + str) {
+                        cb();
+                    }
+                });
+            }
         }
     }
-    dispatch(str) {
+    dispatch(str, useChromeMessaging = false) {
         this._eventDispatched[str] = true;
         console.log(this._key + str)
         document.dispatchEvent(new Event(this._key + str));
+        
+        // Si useChromeMessaging est vrai, envoie aussi un message de Chrome
+        if(useChromeMessaging) {
+            chrome.runtime.sendMessage({ method: this._key + str }, function(response) {
+                console.log(response);
+            });
+        }
     }
 }
