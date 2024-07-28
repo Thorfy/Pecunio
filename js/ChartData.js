742ffa568bcf948e7acaf9bdb3ff158fffdd96b4
Thorfy <lefebvre.remy.pro@gmail.com>
2024-07-28 23:18:54 +0200

    simplify class and format

--- js/ChartData.js
+++ js/ChartData.js
@@ -1,48 +1,42 @@
 class ChartData {
     constructor(transactions, categories, settings) {
-        //console.log(transactions, categories)
-
         this.transactions = transactions;
         this.categories = categories;
         this.settings = settings;
     }
 
     async prepareData() {
-        let filteredTransactions = await this.applySettingOnData();
-        let transactionByCategory = this.mergeTransactionByCategory(filteredTransactions, this.categories);
-        let formattedData = await this.getDataFormated(this.categories, transactionByCategory, true);
-        return formattedData;
+        const filteredTransactions = await this.applySettingOnData();
+        const transactionByCategory = this.mergeTransactionByCategory(filteredTransactions, this.categories);
+        return this.getDataFormated(this.categories, transactionByCategory, true);
     }
 
-    applySettingOnData() {
-        return new Promise(async (resolve, reject) => {
+    async applySettingOnData() {
+        const startDate = Date.parse(settingClass.getSetting('startDate'));
+        const endDate = Date.parse(settingClass.getSetting('endDate'));
+        const returned = [];
 
-            let startDate = Date.parse(settingClass.getSetting('startDate'))
-            let endDate = Date.parse(settingClass.getSetting('endDate'))
-            let accounts = settingClass.getSetting('accounts');
+        for (const transaction of this.transactions) {
+            const transactionDate = new Date(transaction.date);
+            transactionDate.setDate(1);
+            transactionDate.setMonth(transactionDate.getMonth() + transaction.current_month);
+            transaction.date = transactionDate;
 
-            let returned = []
-            for (const transaction of this.transactions) {
-                let transactionDate = new Date(transaction.date);
-                transactionDate.setDate(1);
-                transactionDate.setMonth(transactionDate.getMonth() + transaction.current_month)
-                transaction.date = transactionDate;
-                let modifiedDate = transactionDate.toDateString()
-                if (!(startDate && endDate) || (Date.parse(modifiedDate) >= startDate && Date.parse(modifiedDate) <= endDate)) {
-                    returned.push(transaction)
-                }
+            const modifiedDate = transactionDate.toDateString();
+            if (!(startDate && endDate) || (Date.parse(modifiedDate) >= startDate && Date.parse(modifiedDate) <= endDate)) {
+                returned.push(transaction);
             }
-            resolve(returned)
-        })
+        }
+        return returned;
     }
 
     mergeTransactionByCategory(allTransactions, allCategory) {
-        let preparedData = [];
-        let exceptionCat = [326, 282]
+        const preparedData = [];
+        const exceptionCat = [326, 282];
+
         allTransactions.forEach(transaction => {
             allCategory.forEach(category => {
-                if (!preparedData[category.id])
-                    preparedData[category.id] = [];
+                if (!preparedData[category.id]) preparedData[category.id] = [];
 
                 if (transaction.category.id === category.id && !exceptionCat.includes(transaction.category.id)) {
                     preparedData[category.id].push(transaction);
@@ -51,80 +45,54 @@ class ChartData {
                         if (transaction.category.id === childCategory.id && !exceptionCat.includes(transaction.category.id)) {
                             preparedData[category.id].push(transaction);
                         }
-                    })
+                    });
                 }
-            })
+            });
+        });
 
-        })
         return preparedData;
     }
 
     async getDataFormated(categoryData, transactionByCategory, isCumulative = false) {
-
-        let data = {
-            datasets: []
-        }
-        let settingsList = []
-        await categoryData.forEach(category => {
-            settingsList.push(category.name)
-        })
-        let settings = await chrome.storage.local.get(settingsList)
+        const data = { datasets: [] };
+        const settingsList = categoryData.map(category => category.name);
+        const settings = await chrome.storage.local.get(settingsList);
 
         categoryData.forEach(category => {
+            const transactions = transactionByCategory[parseInt(category.id)];
+            const dateValueObject = [];
+            const transactionObject = {};
 
-            let transactions = transactionByCategory[parseInt(category.id)];
-            let dateValueObject = [];
-            let transactionObject = {};
             transactions.forEach(transaction => {
-
-                // insert control of filter here 
-                //period, account, cumulative
-
-                let dateObj = new Date(transaction.date);
-                //dateObj.setMonth(dateObj.getMonth() + transaction.current_month)
-
-                let month = `${dateObj.getMonth() + 1}`.padStart(2, "0"); //months from 1-12
-                let year = dateObj.getUTCFullYear();
-                const stringDate = [year, month].join("-")
+                const dateObj = new Date(transaction.date);
+                const month = `${dateObj.getMonth() + 1}`.padStart(2, "0");
+                const year = dateObj.getUTCFullYear();
+                const stringDate = [year, month].join("-");
 
                 if (isCumulative) {
-                    if (!transactionObject[stringDate]) {
-                        transactionObject[stringDate] = transaction.amount;
-                    } else {
-                        transactionObject[stringDate] += transaction.amount;
-                    }
+                    transactionObject[stringDate] = (transactionObject[stringDate] || 0) + transaction.amount;
                 } else {
-                    dateValueObject.push({
-                        x: transaction.date,
-                        y: transaction.amount,
-                        name: transaction.name
-                    });
+                    dateValueObject.push({ x: transaction.date, y: transaction.amount, name: transaction.name });
                 }
+            });
 
-            })
             if (isCumulative) {
                 for (const date in transactionObject) {
-                    dateValueObject.push({
-                        x: date,
-                        y: transactionObject[date],
-                    });
+                    dateValueObject.push({ x: date, y: transactionObject[date] });
                 }
             }
-            //let get Config and load it 
-            let dataCategory = {
+
+            data.datasets.push({
                 label: category.name,
                 data: dateValueObject,
                 backgroundColor: this.parseColorCSS("categoryColor_" + category.id),
                 fill: false,
                 tension: 0.3,
                 hidden: settings[category.name]
-            }
+            });
+        });
 
-            data.datasets.push(dataCategory);
-        })
-
-        return data
-
+        return data;
     }
 
     parseColorCSS(strClass) {
@@ -142,21 +110,20 @@ class ChartData {
         const chartJsConfig = await this.getChartJsConfig();
         chartJsConfig.data = formattedData;
 
-        const homeBlock = document.getElementsByClassName("homeBlock");
-        if (homeBlock && homeBlock[0]) {
-            homeBlock[0].innerHTML = "";
-            const canvasDiv = createCanvasElement(homeBlock[0]);
+        const homeBlock = document.getElementsByClassName("homeBlock")[0];
+        if (homeBlock) {
+            homeBlock.innerHTML = "";
+            const canvasDiv = createCanvasElement(homeBlock);
             const ctx = canvasDiv.getContext('2d');
 
-            const myChart = new Chart(ctx, chartJsConfig);
+            new Chart(ctx, chartJsConfig);
         }
     }
 
-
     async getChartJsConfig() {
         const settings = await chrome.storage.local.get(['startDate', 'endDate', 'chartType']);
-        const initialChartType = settings.chartType || 'bar'; // Default to 'bar' if not set
-    
+        const initialChartType = settings.chartType || 'bar';
+
         const commonConfig = {
             responsive: true,
             plugins: {
@@ -206,7 +173,7 @@ class ChartData {
                 }
             }
         };
-    
+
         return {
             type: initialChartType,
             options: {
@@ -221,10 +188,9 @@ class ChartData {
             plugins: [{
                 id: 'toggleTypeChart',
                 beforeInit: async function (chart) {
-                    // Set initial dataset properties based on chart type
                     const newType = initialChartType;
                     chart.config.type = newType;
-    
+
                     chart.data.datasets.forEach(dataset => {
                         dataset.type = newType;
                         if (newType === 'line') {
@@ -238,10 +204,10 @@ class ChartData {
                     });
                 },
                 beforeDraw: function (chart) {
-                    var canvas = chart.canvas;
-                    var ctx = canvas.getContext('2d');
-                    var legendWidth = 120;
-                    var legendHeight = 20;
+                    const canvas = chart.canvas;
+                    const ctx = canvas.getContext('2d');
+                    const legendWidth = 120;
+                    const legendHeight = 20;
                     ctx.save();
                     ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
                     ctx.fillRect(10, 10, legendWidth, legendHeight);
@@ -253,16 +219,16 @@ class ChartData {
                     ctx.restore();
                 },
                 afterEvent: async function (chart, args) {
-                    var event = args.event;
-                    var legendWidth = 120;
-                    var legendHeight = 20;
-                    var withinLegendX = event.x >= 10 && event.x <= 10 + legendWidth;
-                    var withinLegendY = event.y >= 10 && event.y <= 10 + legendHeight;
+                    const event = args.event;
+                    const legendWidth = 120;
+                    const legendHeight = 20;
+                    const withinLegendX = event.x >= 10 && event.x <= 10 + legendWidth;
+                    const withinLegendY = event.y >= 10 && event.y <= 10 + legendHeight;
+
                     if (withinLegendX && withinLegendY && event.type === 'click') {
                         const newType = chart.config.type === 'line' ? 'bar' : 'line';
                         chart.config.type = newType;
-    
-                        // Ensure dataset properties are retained correctly
+
                         chart.data.datasets.forEach(dataset => {
                             dataset.type = newType;
                             if (newType === 'line') {
@@ -274,8 +240,8 @@ class ChartData {
                                 dataset.fill = true;
                             }
                         });
-    
-                        await chrome.storage.local.set({ 'chartType': newType }); // Save the new chart type to settings
+
+                        await chrome.storage.local.set({ 'chartType': newType });
                         chart.update();
                     }
                 }
